13bd128a40308004e34b86404297772524f0dc9b
Chao Pang <ChaoPang229@gmail.com>
2024-10-30 13:02:15 -0400

    updated the bash script and the README

--- src/ehr_foundation_model_benchmark/tutorials/ml4h_demo_2024/run.sh
+++ src/ehr_foundation_model_benchmark/tutorials/ml4h_demo_2024/run.sh
@@ -16,3 +16,46 @@ cp -r $OUTPUT_MEDS_TEMP/metadata $OUTPUT_MEDS
 # Run meds_reader
 pip install meds_reader
 meds_reader_convert $OUTPUT_MEDS $OUTPUT_MEDS_READER
+
+# Run
+sh src/MEDS_DEV/helpers/extract_task.sh $OUTPUT_MEDS OMOP readmission/general_hospital/30d
+
+# Pre-train FEMR
+# Please following the instructions at https://github.com/ChaoPang/femr/tree/omop_meds_v3_tutorial/src/femr/omop_meds_tutorial
+
+# Train GenHPF
+git clone https://github.com/starmpcc/REMed.git
+cd REMed
+conda create -n GenHPF python=3.10
+conda activate GenHPF
+pip install numpy pandas tqdm treelib transformers pyspark polars torch
+pip install performer_pytorch recurrent_memory_transformer_pytorch==0.2.2 transformers==4.30.1 accelerate==0.20.3
+cd src/models/kernels/
+python setup.py install
+
+# Process the cohort
+export MEDS_PATH="set this"
+export GEN_HPF_PROCESSED_MEDS="set this"
+export GEN_HPF_SAVE_DIR="set this"
+python scripts/meds/process_meds.py $MEDS_PATH/data \
+  --metadata $MEDS_PATH/metadata \
+  --cohort $MEDS_PATH/task_labels/readmission/general_hospital/30d/ \
+  --output_dir $GEN_HPF_PROCESSED_MEDS --rebase --workers 4
+
+accelerate launch \
+    --config_file config/config.json \
+    --num_processes 1 \
+    main.py \
+    --src_data meds \
+    --input_path $GEN_HPF_PROCESSED_MEDS \
+    --save_dir $GEN_HPF_SAVE_DIR \
+    --pred_targets meds_single_task \
+    --train_type short \
+    --lr 1e-5 \
+    --n_agg_layers 4 \
+    --pred_dim 128 \
+    --batch_size 16 \
+    --max_seq_len 512 \
+    --dropout 0.3 \
+    --seed 2020 \
+    --patience 5
