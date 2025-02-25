598c27aa27c5f2380aa1d4278215944132d0caef
Chao Pang <ChaoPang229@gmail.com>
2025-02-24 23:27:16 -0500

    lowercased the vocabulary csv file extension

--- src/ehr_foundation_model_benchmark/evaluations/create_codes_to_remove.py
+++ src/ehr_foundation_model_benchmark/evaluations/create_codes_to_remove.py
@@ -1,45 +1,47 @@
+import re
 import pathlib
 import polars as pl
 
 
 def main(args):
     concept = pl.scan_csv(
-        pathlib.Path(args.athena_path) / "CONCEPT.CSV", separator="\t", infer_schema_length=0
+        pathlib.Path(args.athena_path) / "CONCEPT.csv", separator="\t", infer_schema_length=0
     )
     ancestor = pl.scan_csv(
-        pathlib.Path(args.athena_path) / "CONCEPT_ANCESTOR.CSV", separator="\t", infer_schema_length=0
+        pathlib.Path(args.athena_path) / "CONCEPT_ANCESTOR.csv", separator="\t", infer_schema_length=0
     )
     relationship = pl.scan_csv(
-        pathlib.Path(args.athena_path) / "CONCEPT_RELATIONSHIP.CSV", separator="\t", infer_schema_length=0
+        pathlib.Path(args.athena_path) / "CONCEPT_RELATIONSHIP.csv", separator="\t", infer_schema_length=0
     )
     codes_to_remove = pl.scan_csv(args.disease_codes_to_remove, separator=",")
-    code_col = pl.col("vocabulary_id") + "/" + pl.col("concept_code")
+    code_col = (pl.col("vocabulary_id") + "/" + pl.col("concept_code")).alias("code")
 
     icd_codes_to_remove = codes_to_remove.filter(
         pl.col("Source") != "SNOMED"
     )
-    icd_codes_to_remove = icd_codes_to_remove.with_columns(
-        pl.lit("ICD10CM").alias("Source")
-    ).join(
-        concept,
-        left_on=["Source", "Code"],
-        right_on=["vocabulary_id", "concept_code"],
+
+    icd_prefixes = icd_codes_to_remove.collect()["Code"].to_list()
+    pattern = "^(?:" + "|".join(re.escape(prefix) for prefix in icd_prefixes) + ")"
+    icd_codes_to_remove = concept.filter(
+        pl.col("concept_code").str.contains(pattern) & pl.col("vocabulary_id").str.starts_with("ICD10")
     ).select(
-        pl.col("concept_id").alias("non_standard_concept_id")
+        pl.col("concept_id"),
+        code_col
     )
+
     # Get the corresponding concept_id from the concept table
     snomed_codes_to_remove = codes_to_remove.filter(
         pl.col("Source") == "SNOMED"
-    ).with_columns(pl.col("Code").cast(int)).join(
+    ).join(
         concept,
         left_on=["Source", "Code"],
         right_on=["vocabulary_id", "concept_id"],
     ).select(
-        pl.col("concept_id").alias("non_standard_concept_id")
+        pl.col("Code").alias("non_standard_concept_id")
     )
-    all_codes_to_remove = pl.concat([icd_codes_to_remove, snomed_codes_to_remove])
+
     # Get the corresponding standard concept codes
-    all_codes_to_remove = all_codes_to_remove.join(
+    snomed_codes_to_remove = snomed_codes_to_remove.join(
         relationship,
         left_on=["non_standard_concept_id"],
         right_on=["concept_id_1"],
@@ -50,23 +52,31 @@ def main(args):
         pl.col("concept_id_2").alias("standard_concept_id")
     )
     # Get all the descendant concepts
-    all_codes_to_remove = all_codes_to_remove.join(
+    snomed_codes_to_remove = snomed_codes_to_remove.join(
         ancestor,
         left_on=["standard_concept_id"],
         right_on=["ancestor_concept_id"],
-    ).select(
-        pl.col("descendant_concept_id")
-    ).join(
-        concept,
+    )
+    # Map the descendant through concept_relationship to get the source codes
+    snomed_codes_to_remove = snomed_codes_to_remove.join(
+        relationship,
         left_on=["descendant_concept_id"],
+        right_on=["concept_id_2"],
+    ).filter(
+        pl.col("relationship_id") == "Maps to"
+    )
+
+    # Map the source code for the source concept_id
+    snomed_codes_to_remove = snomed_codes_to_remove.join(
+        concept,
+        left_on=["concept_id_1"],
         right_on=["concept_id"],
     ).select(
-        pl.col("concept_id"),
-        pl.col("vocabulary_id"),
-        pl.col("concept_code"),
+        pl.col("concept_id_1").alias("concept_id"),
         code_col
     )
-    all_codes_to_remove.sink_parquet(args.codes_to_remove)
+
+    pl.concat([snomed_codes_to_remove, icd_codes_to_remove]).collect().write_parquet(args.codes_to_remove)
 
 
 if __name__ == "__main__":
