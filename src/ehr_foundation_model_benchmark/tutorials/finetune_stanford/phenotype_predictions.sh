26da0f7212286fcff87285997cd30594f41c1b94
ykobaya9 <yk3043@cumc.columbia.edu>
2025-04-16 18:05:35 -0400

    add readme

--- src/ehr_foundation_model_benchmark/tutorials/finetune_stanford/phenotype_predictions.sh
+++ src/ehr_foundation_model_benchmark/tutorials/finetune_stanford/phenotype_predictions.sh
@@ -2,7 +2,7 @@
 
 # List of tasks
 tasks=("AMI" "Celiac" "CLL" "HTN" "Ischemic_Stroke" "MASLD" "Osteoporosis" "Pancreatic_Cancer" "SLE" "T2DM")
-train_sizes=(1000 5000 10000 50000 100000)
+train_sizes=(1000 10000 100000)
 model_type="mamba-ehrshot"
 
 # Loop through each task and run main.py with --task
