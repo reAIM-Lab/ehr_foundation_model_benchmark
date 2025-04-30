372edc456409d2f6f98be8ba046db88099a3e091
ykobaya9 <yk3043@cumc.columbia.edu>
2025-04-30 16:54:05 -0400

    more efficient batching

--- src/ehr_foundation_model_benchmark/tutorials/finetune_stanford/phenotype_predictions.sh
+++ src/ehr_foundation_model_benchmark/tutorials/finetune_stanford/phenotype_predictions.sh
@@ -10,9 +10,9 @@ for task in "${tasks[@]}"; do
     echo "Running task: $task"
     python main.py --task "$task" --model_type $model_type
 
-    for train_size in "${train_sizes[@]}"; do
-        meds-evaluation-cli \
-            predictions_path="/data/mchome/yk3043/ehr_foundation_model_benchmark/src/ehr_foundation_model_benchmark/tutorials/finetune_stanford/predictions/${task}/${model_type}_${train_size}.parquet" \
-            output_dir="outputs/${task}/${train_size}"
-    done
+    # for train_size in "${train_sizes[@]}"; do
+    #     meds-evaluation-cli \
+    #         predictions_path="/data/mchome/yk3043/ehr_foundation_model_benchmark/src/ehr_foundation_model_benchmark/tutorials/finetune_stanford/predictions/${task}/${model_type}_${train_size}.parquet" \
+    #         output_dir="outputs/${task}/${train_size}"
+    # done
 done
