b1182c9f0a1e451a050012d027b7a2f39e90951f
ykobaya9 <46499988+ykobaya9@users.noreply.github.com>
2025-05-13 16:05:06 -0400

    anonymized (#17)

--- src/ehr_foundation_model_benchmark/tools/icd_mapping/run.sh
+++ src/ehr_foundation_model_benchmark/tools/icd_mapping/run.sh
@@ -2,12 +2,12 @@
 
 # Activate Conda environment
 source /opt/anaconda3/etc/profile.d/conda.sh
-conda activate MEDS
+conda activate env_name
 
-export INPUT_MEDS="/data2/processed_datasets/ehr_foundation_data/ohdsi_cumc_deid/ohdsi_cumc_deid_2023q4r3_meds_v3_with_unit_discharge"
-export OUTPUT_MEDS_TEMP="/data2/processed_datasets/ehr_foundation_data/ohdsi_cumc_deid/temp_icd_conversion/"
+export INPUT_MEDS=""
+export OUTPUT_MEDS_TEMP=""
 
 # Generate a sample of columbia data
-PYTHONPATH=./:$PYTHONPATH python sample_columbia_meds.py --input_meds $INPUT_MEDS --output_meds $OUTPUT_MEDS_TEMP
+PYTHONPATH=./:$PYTHONPATH python sample_meds.py --input_meds $INPUT_MEDS --output_meds $OUTPUT_MEDS_TEMP
 PYTHONPATH=./:$PYTHONPATH python compute_prevalences.py --input_meds $INPUT_MEDS --output_meds $OUTPUT_MEDS_TEMP 
 PYTHONPATH=./:$PYTHONPATH python map_icd.py --input_meds $INPUT_MEDS --output_meds $OUTPUT_MEDS_TEMP --source_vocabulary "ICD9" --target_vocabulary "ICD10"
